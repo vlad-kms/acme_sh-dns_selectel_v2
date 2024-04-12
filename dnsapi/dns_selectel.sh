ed36c0782fb2858c7e4a16202e23c435040c70e1
Vladimir Alexeev <9141778236@mai.ru>
2024-04-13 08:19:31 +1000

    v1.0.9

--- dnsapi/dns_selectel.sh
+++ dnsapi/dns_selectel.sh
@@ -14,6 +14,8 @@ SL_Api="https://api.selectel.ru/domains"
 
 auth_uri="https://cloud.api.selcloud.ru/identity/v3/auth/tokens"
 
+_sl_sep='#'
+
 ########  Public functions #####################
 
 #Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
@@ -21,9 +23,15 @@ dns_selectel_add() {
   fulldomain=$1
   txtvalue=$2
 
+  #if ! _sl_init_vars; then
   if ! _sl_init_vars; then
     return 1
   fi
+  _debug2 SL_Ver "$SL_Ver"
+  _debug2 SL_Expire "$SL_Expire"
+  _debug2 SL_Login_Name "$SL_Login_Name"
+  _debug2 SL_Login_ID "$SL_Login_ID"
+  _debug2 SL_Project_Name "$SL_Project_Name"
 
   _debug "First detect the root zone"
   if ! _get_root "$fulldomain"; then
@@ -35,10 +43,27 @@ dns_selectel_add() {
   _debug _domain "$_domain"
 
   _info "Adding record"
-  if _sl_rest POST "/$_domain_id/records/" "{\"type\": \"TXT\", \"ttl\": 60, \"name\": \"$fulldomain\", \"content\": \"$txtvalue\"}"; then
+  if [[ "$SL_Ver" == "v2" ]]; then
+    _ext_uri="/zones/$_domain_id/rrset"
+    _text_tmp=$(echo $txtvalue | sed -En "s/[\"]*([^\"]*)/\1/p")
+    _debug txtvalue "$txtvalue"
+    _debug _text_tmp "$_text_tmp"
+    _data="{\"type\": \"TXT\", \"ttl\": 60, \"name\": \"${fulldomain}.\", \"records\": [{\"content\":$_text_tmp}]}"
+  elif [[ "$SL_Ver" == "v1" ]]; then
+    _ext_uri="/$_domain_id/records/"
+    _data="{\"type\":\"TXT\",\"ttl\":60,\"name\":\"$fulldomain\",\"content\":\"$txtvalue\"}"
+  else
+    #not valid
+    _err "Error. Unsupported version API $SL_Ver"
+    return 1
+  fi
+  _debug3 _ext_uri "$_ext_uri"
+  _debug3 _data "$_data"
+
+  if _sl_rest POST "$_ext_uri" "$_data"; then
     if _contains "$response" "$txtvalue" || _contains "$response" "record_already_exists"; then
       _info "Added, OK"
-      return 0
+      return 1
     fi
   fi
   _err "Add txt record error."
@@ -106,44 +131,105 @@ dns_selectel_rm() {
 _get_root() {
   domain=$1
 
-  if ! _sl_rest GET "/"; then
-    return 1
-  fi
-
-  i=2
-  p=1
-  while true; do
-    h=$(printf "%s" "$domain" | cut -d . -f $i-100)
-    _debug h "$h"
-    if [ -z "$h" ]; then
-      #not valid
+  if [[ "$SL_Ver" == 'v1' ]]; then
+    # version API 1
+    if ! _sl_rest GET "/"; then
       return 1
     fi
-
-    if _contains "$response" "\"name\" *: *\"$h\","; then
-      _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
-      _domain=$h
-      _debug "Getting domain id for $h"
-      if ! _sl_rest GET "/$h"; then
+    i=2
+    p=1
+    while true; do
+      h=$(printf "%s" "$domain" | cut -d . -f $i-100)
+      _debug h "$h"
+      if [ -z "$h" ]; then
+        #not valid
         return 1
       fi
-      _domain_id="$(echo "$response" | tr "," "\n" | tr "}" "\n" | tr -d " " | grep "\"id\":" | cut -d : -f 2)"
-      return 0
+
+      if _contains "$response" "\"name\" *: *\"$h\","; then
+        _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
+        _domain=$h
+        _debug "Getting domain id for $h"
+        if ! _sl_rest GET "/$h"; then
+        _err "Error read records of all domains $SL_Ver"
+          return 1
+        fi
+        _domain_id="$(echo "$response" | tr "," "\n" | tr "}" "\n" | tr -d " " | grep "\"id\":" | cut -d : -f 2)"
+        return 0
+      fi
+      p=$i
+      i=$(_math "$i" + 1)
+    done
+    _err "Error read records of all domains $SL_Ver"
+    return 1
+  elif [[ "$SL_Ver" == "v2" ]]; then
+    # version API 2
+    _ext_uri='zones/'
+    domain="${domain}."
+    _debug "domain:: " "$domain"
+    # read records of all domains
+    if ! _sl_rest GET "$_ext_uri"; then
+        #not valid
+      _err "Error read records of all domains $SL_Ver"
+      return 1
     fi
-    p=$i
-    i=$(_math "$i" + 1)
-  done
-  return 1
+    i=2
+    p=1
+    while true; do
+      h=$(printf "%s" "$domain" | cut -d . -f $i-100)
+      _debug h "$h"
+      if [ -z "$h" ]; then
+        #not valid
+        _err "The domain was not found among the registered ones"
+        return 1
+      fi
+
+      _domain_record=$(echo $response | sed -En "s/.*(\{[^}]*id[^}]*\"name\" *: *\"$h\"[^}]*}).*/\1/p")
+      _debug "_domain_record:: " "$_domain_record"
+      if [[ -n $_domain_record ]]; then
+        _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
+        _domain=$h
+        _debug "Getting domain id for $h"
+        #_domain_id="$(echo "$_domain_record" | tr "," "\n" | tr "}" "\n" | tr -d " " | grep "\"id\":" | cut -d : -f 2 | sed -En "s/\"([^\"]*)\"/\1\p")"
+        _domain_id=$(echo $_domain_record | sed -En "s/\{[^}]*\"id\" *: *\"([^\"]*)\"[^}]*\}/\1/p")
+        return 0
+      fi
+      p=$i
+      i=$(_math "$i" + 1)
+    done
+    #not valid
+    _err "Error read records of all domains $SL_Ver"
+    return 1
+  else
+    #not valid
+    _err "Error. Unsupported version API $SL_Ver"
+    return 1
+  fi
 }
 
+#################################################################
+# use: method add_url body
 _sl_rest() {
   m=$1
   ep="$2"
   data="$3"
-  _debug "$ep"
 
-  export _H1="X-Token: $SL_Key"
+  _token=$(_get_auth_token)
+  #_debug "$_token"
+  if [[ -z $_token ]]; then
+    _err "BAD key or token $ep"
+    return 1
+  fi
+  if [[ $SL_Ver == v2 ]]; then
+    _h1_name="X-Auth-Token"
+  else
+    _h1_name='X-Token'
+  fi
+  export _H1="${_h1_name}: ${_token}"
   export _H2="Content-Type: application/json"
+  _debug3 "Full URI: " "$SL_Api/$SL_Ver/$ep"
+  _debug3 "_H1:" "$_H1"
+  _debug3 "_H2:" "$_H2"
 
   if [ "$m" != "GET" ]; then
     _debug data "$data"
@@ -160,7 +246,67 @@ _sl_rest() {
   return 0
 }
 
+#################################################################3
+# use: 
+_get_auth_token() {
+  if [[ $SL_Ver == 'v1' ]]; then
+    # token for v1
+    _token_keystone=$SL_Key
+  elif [[ $SL_Ver == 'v2' ]]; then
+    # token for v2. Get a token for calling the API
+    token_v2=$(_readaccountconf_mutable SL_Token_V2)
+    if [[ -n $token_v2 ]]; then
+      # The structure with the token was considered. Let's check its validity
+      # field 1 - SL_Login_Name
+      # field 2 - token keystone
+      # field 3 - SL_Login_ID
+      # field 4 - SL_Project_Name
+      # field 5 - Receipt time
+      # separator - ';'
+      _login_name=$(_getfield $token_v2 1 "$_sl_sep")
+      _token_keystone=$(_getfield $token_v2 2 "$_sl_sep")
+      _project_name=$(_getfield $token_v2 4 "$_sl_sep")
+      _receipt_time=$(_getfield $token_v2 5 "$_sl_sep")
+      _login_id=$(_getfield $token_v2 3 "$_sl_sep")
+      _debug3 _login_name $_login_name
+      _debug3 _login_id $_login_id
+      _debug3 _project_name $_project_name
+      _debug3 _receipt_time "$(date -d @$_receipt_time -u)"
+      
+      # check the validity of the token for the user and the project and its lifetime
+      _dt_diff_minute=$(( ( $EPOCHSECONDS-$_receipt_time )/60 ))
+      _debug3 _dt_diff_minute "$_dt_diff_minute"
+      (( $_dt_diff_minute > $SL_Expire )) && unset _token_keystone
+      if [[ $_project_name != $SL_Project_Name ]] || [[ $_login_name != $SL_Login_Name ]] || [[ $_login_id != $SL_Login_ID ]]; then
+        unset _token_keystone
+      fi
+    fi
+    if [[ -z $_token_keystone ]]; then
+      # the previous token is incorrect or was not received, get a new one
+      _debug "Update token"
+      _data_auth="{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"${SL_Login_Name}\",\"domain\":{\"name\":\"${SL_Login_ID}\"},\"password\":\"${SL_Pswd}\"}}},\"scope\":{\"project\":{\"name\":\"${SL_Project_Name}\",\"domain\":{\"name\":\"${SL_Login_ID}\"}}}}}"
+      #_secure_debug2 "_data_auth" "$_data_auth"
+      export _H1="Content-Type: application/json"
+      # body  url [needbase64] [POST|PUT|DELETE] [ContentType]
+      _result=$(_post  $_data_auth $auth_uri)
+      _token_keystone=$(cat "$HTTP_HEADER" | grep 'x-subject-token' | sed -nE "s/[[:space:]]*x-subject-token:[[:space:]]*([[:print:]]*)(\r*)/\1/p")
+      echo $_token_keystone > /root/123456.qwe
+      _dt_curr=$EPOCHSECONDS
+      SL_Token_V2="${SL_Login_Name}${_sl_sep}${_token_keystone}${_sl_sep}${SL_Login_ID}${_sl_sep}${SL_Project_Name}${_sl_sep}${_dt_curr}"
+      _saveaccountconf_mutable SL_Token_V2 "$SL_Token_V2"
+    fi
+  else
+    # token set empty for unsupported version API
+    _token_keystone=""
+  fi
+  printf -- "%s" $_token_keystone
+}
+
+#################################################################
+# use: [non_save]
 _sl_init_vars() {
+  _non_save=${1}
+ _debug2 _non_save "$_non_save"
 
   _debug "First init variables"
   # version API
@@ -171,8 +317,7 @@ _sl_init_vars() {
     _err "Please specify you API version and try again."
     return 1
   fi
-  #_saveaccountconf_mutable SL_Ver "$SL_Ver"
- _debug2 SL_Ver "$SL_Ver"
+  _debug2 SL_Ver "$SL_Ver"
  
   if [[ "$SL_Ver" == "v1" ]]; then
     # token
@@ -185,7 +330,9 @@ _sl_init_vars() {
       return 1
     fi
     #save the api key to the account conf file.
-    _saveaccountconf_mutable SL_Key "$SL_Key"
+    if [[ -z $_non_save ]]; then
+      _saveaccountconf_mutable SL_Key "$SL_Key"
+    fi
   elif [[ "$SL_Ver" == "v2" ]]; then
     # time expire token
     SL_Expire="${SL_Expire:-$(_readaccountconf_mutable SL_Expire)}"
@@ -194,8 +341,9 @@ _sl_init_vars() {
       def_str=' (use default)'
       SL_Expire=1400 # 23h 20 min
     fi
-    _saveaccountconf_mutable SL_Expire "$SL_Expire"
-    _debug2 SL_Expire "$SL_Expire"
+    if [[ -z $_non_save ]]; then
+      _saveaccountconf_mutable SL_Expire "$SL_Expire"
+    fi
     # login service user
     SL_Login_Name="${SL_Login_Name:-$(_readaccountconf_mutable SL_Login_Name)}"
     if [ -z "$SL_Login_Name" ]; then
@@ -204,8 +352,9 @@ _sl_init_vars() {
       _err "Please provide a service user name and try again."
       return 1
     fi
-    _saveaccountconf_mutable SL_Login_Name "$SL_Login_Name"
-    _debug2 SL_Login_Name "$SL_Login_Name"
+    if [[ -z $_non_save ]]; then
+      _saveaccountconf_mutable SL_Login_Name "$SL_Login_Name"
+    fi
     # user ID
     SL_Login_ID="${SL_Login_ID:-$(_readaccountconf_mutable SL_Login_ID)}"
     if [ -z "$SL_Login_ID" ]; then
@@ -214,8 +363,9 @@ _sl_init_vars() {
       _err "Please provide a user ID and try again."
       return 1
     fi
-    _saveaccountconf_mutable SL_Login_ID "$SL_Login_ID"
-    _debug2 SL_Login_ID "$SL_Login_ID"
+    if [[ -z $_non_save ]]; then
+      _saveaccountconf_mutable SL_Login_ID "$SL_Login_ID"
+    fi
     # project name
     SL_Project_Name="${SL_Project_Name:-$(_readaccountconf_mutable SL_Project_Name)}"
     if [ -z "$SL_Project_Name" ]; then
@@ -224,8 +374,9 @@ _sl_init_vars() {
       _err "Please provide a project name and try again."
       return 1
     fi
-    _saveaccountconf_mutable SL_Project_Name "$SL_Project_Name"
-    _debug2 SL_Project_Name "$SL_Project_Name"
+    if [[ -z $_non_save ]]; then
+      _saveaccountconf_mutable SL_Project_Name "$SL_Project_Name"
+    fi
     # service user password
     SL_Pswd="${SL_Pswd:-$(_readaccountconf_mutable SL_Pswd)}"
     if [ -z "$SL_Pswd" ]; then
@@ -234,9 +385,9 @@ _sl_init_vars() {
       _err "Please provide a service user password and try again."
       return 1
     fi
-    _saveaccountconf_mutable SL_Pswd "$SL_Pswd"
-    _debug2 SL_Pswd "$SL_Pswd"
-    return 1
+    if [[ -z $_non_save ]]; then
+      _saveaccountconf_mutable SL_Pswd "$SL_Pswd" "12345678"
+    fi
   else
     SL_Ver=""
     _err "You also specified the wrong version of the selectel.ru API."
@@ -244,7 +395,9 @@ _sl_init_vars() {
     return 1
   fi
 
-  _saveaccountconf_mutable SL_Ver "$SL_Ver"
+  if [[ -z $_non_save ]]; then
+    _saveaccountconf_mutable SL_Ver "$SL_Ver"
+  fi
 
   return 0
 }
